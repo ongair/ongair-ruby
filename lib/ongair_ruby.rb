d96fa995543cce6309bd5c727e68cec34b34dbb5
root <root@localhost.localdomain>
2015-02-09 17:09:56 +0300

    Added method to send Whatsapp messages

--- lib/ongair_ruby.rb
+++ lib/ongair_ruby.rb
@@ -1,7 +1,9 @@
 require "ongair_ruby/version"
+require "httparty"
 
 module OngairRuby
   module_function
-
-  
+  def send(url, phone_number, message, thread=true)
+  	HTTParty.post(url, body: {token: ENV['ONGAIR_API_TOKKEN'], phone_number: phone_number, text: message, thread: thread })
+  end  
 end

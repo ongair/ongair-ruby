38e267c32916b2bf286561e738bc807ccda5f832
muaad <muadh24@gmail.com>
2015-02-13 17:38:51 +0300

    Added way to close conversation using the gem

--- lib/ongair_ruby.rb
+++ lib/ongair_ruby.rb
@@ -5,6 +5,10 @@ module OngairRuby
   module_function
 
   def send_message(phone_number, message, thread=true)
-  	HTTParty.post(ENV['ONGAIR_URL'], body: {token: ENV['ONGAIR_API_TOKKEN'], phone_number: phone_number, text: message, thread: thread })
+  	HTTParty.post("#{ENV['ONGAIR_URL']}/send", body: {token: ENV['ONGAIR_API_TOKKEN'], phone_number: phone_number, text: message, thread: thread })
+  end
+
+  def close_conversation(phone_number)
+  	HTTParty.post("#{ENV['ONGAIR_URL']}/conversations/#{phone_number}/close", body: {token: ENV['ONGAIR_API_TOKKEN'], phone_number: phone_number })
   end
 end
